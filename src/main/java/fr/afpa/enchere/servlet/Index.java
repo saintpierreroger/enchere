438d584ef91d6f00abc20d015540d81d6ca8265d
Thomas Fischer <92039475+thomas-c-p-fischer@users.noreply.github.com>
2022-07-11 13:05:10 +0200

    perso

--- src/main/java/fr/afpa/enchere/servlet/Index.java
+++ src/main/java/fr/afpa/enchere/servlet/Index.java
@@ -1,5 +1,7 @@
 package fr.afpa.enchere.servlet;
 
+import fr.afpa.enchere.bo.Articles_Vendus;
+import fr.afpa.enchere.bo.UtilisateursArticles_Vendus;
 import fr.afpa.enchere.dal.ArticleSQL;
 import fr.afpa.enchere.dal.CategorieSQL;
 import fr.afpa.enchere.dal.EnchereSQL;
@@ -10,6 +12,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/index")
 public class Index extends HttpServlet {
@@ -25,13 +29,16 @@ public class Index extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         // Utilisation de la DAL
         ArticleSQL articleSQL = new ArticleSQL();
-        String filtre = request.getParameter("filter");
-
+        List<UtilisateursArticles_Vendus> articles = articleSQL.selectByNomArticleEtNoCategorie(
+                request.getParameter("name"),
+                Integer.parseInt(request.getParameter("no_categorie")));
+        // Récupération du paramétre de la jsp
+        String name = request.getParameter("name");
+        String filter = request.getParameter("no_categorie");
         // Recherche d'un article par son nom mais pas affichage pour l'instant
-        request.setAttribute("articles", articleSQL.selectByNomArticleEtNoCategorie("name", Integer.parseInt(filtre)));
+        request.setAttribute("articles", articles);
         // Envoi à index.jsp
         request.getRequestDispatcher("WEB-INF/index.jsp").forward(request, response);
 
